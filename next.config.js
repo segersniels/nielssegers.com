83dd40cfbf8e09c4cd8d6677096abd5a910f6d94
segersniels <segers.n@hotmail.com>
2019-11-04 17:09:13 +0100

    :fire: Remove unused requires

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const css = require('@zeit/next-css');
 const optimizedImages = require('next-optimized-images');
 const fonts = require('next-fonts');
-const client = require('./sanity');
 
 const webpack = config => {
   config.plugins = config.plugins || [];
