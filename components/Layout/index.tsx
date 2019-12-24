46f523698a1920d18cb7e06f571fb0e7b93bf208
segersniels <segers.n@hotmail.com>
2019-12-24 16:23:47 +0100

    :loud_sound: Adjust head with content

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -5,6 +5,7 @@ import Head from 'next/head';
 import { Fragment, ReactNode } from 'react';
 import 'styles/core.css';
 import { Container, Wrapper } from './styles';
+import { useRouter } from 'next/router';
 
 interface Props {
   children: ReactNode;
@@ -13,6 +14,8 @@ interface Props {
 
 const Layout = (props: Props) => {
   const { children, className = 'Layout' } = props;
+  const router = useRouter();
+  const { asPath } = router;
 
   return (
     <Fragment>
@@ -23,12 +26,31 @@ const Layout = (props: Props) => {
           name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
         />
-        <meta key="description" name="description" />
 
-        <meta key="og:sitename" property="og:sitename" />
-        <meta key="og:title" property="og:title" />
-        <meta key="og:description" property="og:description" />
-        <meta key="og:url" property="og:url" />
+        <meta key="description" name="description" />
+        <meta key="og:sitename" property="og:sitename" content="Niels Segers" />
+        <meta key="og:title" property="og:title" content="Niels Segers" />
+        <meta
+          key="og:description"
+          property="og:description"
+          content="Personal website"
+        />
+        <meta
+          key="og:image"
+          property="og:image"
+          content={`${process.env.URL}/static/images/avatar.jpg`}
+        />
+
+        <link
+          key="canonical"
+          rel="canonical"
+          href={`${process.env.URL}${asPath}`}
+        />
+        <meta
+          key="og:url"
+          property="og:url"
+          content={`${process.env.URL}${asPath}`}
+        />
 
         <link href="https://twitter.com/segersniels_" rel="me" />
         <link
