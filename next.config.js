619c7beb1a781f3146f784feed837c0f77f69da7
segersniels <segers.n@hotmail.com>
2020-05-11 21:58:37 +0200

    :arrow_up: Upgrade next@9.4 and remove no longer needed dependencies

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
-require('dotenv').config();
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-const Dotenv = require('dotenv-webpack');
-const path = require('path');
 
-const webpack = (config) => {
-  config.plugins = config.plugins || [];
-
-  config.plugins = [
-    ...config.plugins,
-    new Dotenv({
-      path: path.join(__dirname, '.env'),
-      systemvars: true,
-    }),
-  ];
-
-  return config;
-};
-
-module.exports = withPlugins([[optimizedImages]], {
-  webpack,
-});
+module.exports = withPlugins([[optimizedImages]]);
