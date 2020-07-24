4b668c2798335a70daa81722d63afb1b5cee88d7
segersniels <segers.n@hotmail.com>
2020-07-24 23:50:51 +0200

    :heavy_plus_sign: Add eslint-plugin-simple-import-sort@5.0.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,14 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:react-hooks/recommended',
   ],
+  plugins: ['simple-import-sort'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-var-requires': 'off',
+    'simple-import-sort/sort': 'error',
   },
   settings: {
     react: {
