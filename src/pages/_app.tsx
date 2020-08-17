6db6613c009e1c2a287debc7d7de9bda8437133d
segersniels <segers.n@hotmail.com>
2020-08-17 18:41:40 +0000

    :sparkles: add route change analytic tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,24 @@ import 'styles/core.css';
 
 import { PageProvider } from 'context/PageContext';
 import { AppProps } from 'next/app';
-import React from 'react';
+import { Router } from 'next/router';
+import React, { useEffect } from 'react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      (window as any).gtag('config', process.env.NEXT_PUBLIC_TRACKING_ID, {
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        page_path: url,
+      });
+    };
+
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (
     <PageProvider>
       <Component {...pageProps} />
