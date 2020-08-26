f4838dcda31532f8caa9f3b19663615b87c64177
segersniels <segers.n@hotmail.com>
2020-08-26 10:59:40 +0200

    :fire: Remove unused shared components

--- src/styles/shared.ts
+++ src/styles/shared.ts
@@ -14,56 +14,6 @@ export const SubTitle = styled.h2<{ color?: string }>`
   }}
 `;
 
-export const Text = styled.p`
-  font-size: 1.8rem;
-  font-weight: 300;
-  margin: 0;
-`;
-
-export const Redirect = styled.a<{
-  underline?: boolean;
-  background?: string;
-  color?: string;
-}>`
-  text-decoration: none;
-  cursor: pointer;
-
-  ${({ underline }) => {
-    if (underline) {
-      return css`
-        text-decoration: underline;
-      `;
-    }
-    return;
-  }}
-
-  ${({ background }) => {
-    if (background) {
-      return css`
-        background-color: ${background};
-        padding-left: 1rem;
-        padding-right: 1rem;
-        width: fit-content;
-
-        display: inline-flex;
-
-        transform-origin: top;
-        transform: skewX(30deg);
-      `;
-    }
-    return;
-  }}
-
-${({ color }) => {
-  if (color) {
-    return css`
-      color: ${color};
-    `;
-  }
-  return;
-}}
-`;
-
 export const ViewportAwareContainer = styled.div`
   margin-top: 7rem;
   margin-bottom: 7rem;
