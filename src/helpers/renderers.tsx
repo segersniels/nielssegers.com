978a38228660ecccd8a30f7addbece21686e8e83
segersniels <segers.n@hotmail.com>
2021-01-12 18:17:23 +0100

    :recycle: Use custom responsive image component instead of defining it here

--- src/helpers/renderers.tsx
+++ src/helpers/renderers.tsx
@@ -1,51 +1,8 @@
 /* eslint-disable react/display-name */
+import Image from '@segersniels/image';
 import Window from '@segersniels/window';
-import NextImage from 'next/image';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-
-const ResponsiveImage = ({
-  src,
-  alt,
-  title,
-}: {
-  src: string;
-  alt: string;
-  title: string;
-}) => {
-  const [width, setWidth] = useState<null | number>(null);
-  const [height, setHeight] = useState<null | number>(null);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    if (loaded) {
-      return;
-    }
-
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      setWidth(img.naturalWidth);
-      setHeight(img.naturalHeight);
-      setLoaded(true);
-    };
-  }, [src, height, width, loaded]);
-
-  if (!loaded || !height || !width) {
-    return null;
-  }
-
-  return (
-    <NextImage
-      src={src}
-      quality="85"
-      width={width}
-      height={height}
-      alt={alt}
-      title={title}
-    />
-  );
-};
 
 const Wrapper = styled.div`
   text-align: center;
@@ -64,7 +21,7 @@ export default {
   }) => {
     return (
       <Wrapper>
-        <ResponsiveImage src={src} alt={alt} title={title} />
+        <Image src={src} alt={alt} title={title} />
       </Wrapper>
     );
   },
