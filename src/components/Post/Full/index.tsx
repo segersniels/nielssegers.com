8c9a3dd8bc85f90dcd57e82e9c8191443a3b7a36
segersniels <segers.n@hotmail.com>
2021-01-12 13:55:11 +0100

    :sparkles: Show cover image when embedding

--- src/components/Post/Full/index.tsx
+++ src/components/Post/Full/index.tsx
@@ -6,12 +6,18 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { SubTitle } from 'styles/shared';
 
-const CustomHead = (props: { title: string; excerpt: string }) => {
-  const { title, excerpt } = props;
+const CustomHead = (props: {
+  title: string;
+  excerpt: string;
+  coverImage: string;
+}) => {
+  const { title, excerpt, coverImage } = props;
+
   return (
     <Head>
       <meta key="og:title" property="og:title" content={title} />
       <meta key="og:description" property="og:description" content={excerpt} />
+      <meta key="og:image" property="og:image" content={coverImage} />
 
       <meta key="twitter:title" property="twitter:title" content={title} />
       <meta
@@ -19,21 +25,17 @@ const CustomHead = (props: { title: string; excerpt: string }) => {
         property="twitter:description"
         content={excerpt}
       />
-      <meta
-        key="twitter:card"
-        name="twitter:card"
-        content="summary_large_image"
-      />
+      <meta key="twitter:card" name="twitter:card" content={coverImage} />
     </Head>
   );
 };
 
 const Item = (props: Partial<Post>) => {
-  const { title, content, excerpt } = props;
+  const { title, content, excerpt, coverImage } = props;
 
   return (
     <Container>
-      <CustomHead excerpt={excerpt} title={title} />
+      <CustomHead excerpt={excerpt} title={title} coverImage={coverImage} />
       <SubTitle>{title}</SubTitle>
       <ReactMarkdown source={content} renderers={renderers} />
     </Container>
