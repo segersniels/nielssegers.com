d579fd1752dcdc91e447eb0940f65301b326ebde
segersniels <segers.n@hotmail.com>
2021-01-12 13:47:56 +0100

    :ambulance: Deprecated unsized prop

--- src/helpers/renderers.tsx
+++ src/helpers/renderers.tsx
@@ -13,5 +13,20 @@ export default {
     alt?: string;
     src?: string;
     title?: string;
-  }) => <Image alt={alt} src={src} title={title} unsized />,
+  }) => {
+    // https://cdn.sanity.io/images/9coakvkk/production/6c82d42a7f5cf54f6c505ae95064ae1b98423a22-3024x4032.jpg
+    const filename = /[^/]*$/.exec(src)[0];
+    const [width, height] = filename.split('-')[1].split('.')[0].split('x');
+
+    return (
+      <Image
+        alt={alt}
+        src={src}
+        title={title}
+        layout="intrinsic"
+        height={height}
+        width={width}
+      />
+    );
+  },
 };
