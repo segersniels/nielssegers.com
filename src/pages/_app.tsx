6355083f63694e96298f42899df0ab7d62495a04
segersniels <segers.n@hotmail.com>
2021-01-17 01:24:48 +0100

    :fire: Remove google analytics support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,10 @@
 import { PageProvider } from 'context/PageContext';
 import { AppProps } from 'next/app';
-import { Router } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle, { theme } from 'styles/global';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      (window as any).gtag(
-        'config',
-        process.env.NEXT_PUBLIC_ANALYTICS_TRACKING_ID,
-        {
-          // eslint-disable-next-line @typescript-eslint/camelcase
-          page_path: url,
-        },
-      );
-    };
-
-    Router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, []);
-
   return (
     <>
       <GlobalStyle />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document from 'next/document';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -28,34 +28,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_TRACKING_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
   }
 }
