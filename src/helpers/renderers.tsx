90be5fe7955d550d50a7b4035e00120e1fbca290
segersniels <segers.n@hotmail.com>
2021-03-09 19:33:10 +0100

    :wrench: Configure custom cloudinary image loader

--- src/helpers/renderers.tsx
+++ src/helpers/renderers.tsx
@@ -5,11 +5,17 @@ import React from 'react';
 const Window = dynamic(() => import('@segersniels/window'));
 const Image = dynamic(() => import('@segersniels/image'));
 
+const Loader = ({ src, width, quality }) => {
+  return `https://res.cloudinary.com/dmzjyuyyv/image/fetch/w_${width},q_${
+    quality || 75
+  }/${src}`;
+};
+
 export default {
   code: (props: any) => <Window {...props} />,
   image: (props: any) => (
     <div style={{ textAlign: 'center' }}>
-      <Image {...props} priority />
+      <Image {...props} priority loader={Loader} />
     </div>
   ),
 };
