253acc13d2c0fa5f8e4ae547919617ae37fe8d6d
segersniels <segers.n@hotmail.com>
2021-03-10 01:55:56 +0100

    :art: Reorganise for readability

--- src/helpers/renderers.tsx
+++ src/helpers/renderers.tsx
@@ -2,10 +2,10 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
+const Image = dynamic(() => import('@segersniels/image'));
 const Window = dynamic(() => import('@segersniels/window'), {
   ssr: false,
 });
-const Image = dynamic(() => import('@segersniels/image'));
 
 const Loader = ({ src, width, quality }) => {
   return `https://res.cloudinary.com/dmzjyuyyv/image/fetch/f_auto,w_${width},q_${
