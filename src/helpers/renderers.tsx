0575c0c776ae6b6804e455d05f8613da066182a5
segersniels <segers.n@hotmail.com>
2021-03-30 12:36:43 +0200

    :fire: Remove custom cloudinary loader

--- src/helpers/renderers.tsx
+++ src/helpers/renderers.tsx
@@ -4,17 +4,12 @@ import dynamic from 'next/dynamic';
 import React from 'react';
 
 const Image = dynamic(() => import('@segersniels/image'));
-const Loader = ({ src, width, quality }) => {
-  return `https://res.cloudinary.com/dmzjyuyyv/image/fetch/f_auto,w_${width},q_${
-    quality || 75
-  }/${src}`;
-};
 
 export default {
   code: (props: any) => <Window {...props} />,
   image: (props: any) => (
     <div style={{ textAlign: 'center' }}>
-      <Image {...props} priority loader={Loader} />
+      <Image {...props} priority />
     </div>
   ),
 };
